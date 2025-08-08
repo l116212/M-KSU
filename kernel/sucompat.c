b8be42c90a223397887c56746e865118bec98e93
zeng <2011294202@qq.com>
2025-08-09 04:01:43 +0800

    更新版本号

--- kernel/sucompat.c
+++ kernel/sucompat.c
@@ -16,7 +16,7 @@
 #include "objsec.h"
 #include "allowlist.h"
 #include "arch.h"
-#include "klog.h" 
+#include "klog.h"
 #include "ksud.h"
 #include "kernel_compat.h"
 
@@ -25,22 +25,37 @@
 #define SU_PATH "/system/bin/su"
 #define SH_PATH "/system/bin/sh"
 
+// 声明外部函数和变量
 extern void ksu_escape_to_root();
-extern bool ksu_devpts_hook;
 extern u32 ksu_devpts_sid;
 
-static const char sh_path[] = "/system/bin/sh";
+// 当 CONFIG_KSU_KPROBES_HOOK 未定义时，使用非Kprobes模式
+#ifndef CONFIG_KSU_KPROBES_HOOK
+static bool ksu_sucompat_non_kp __read_mostly = true;
+#endif
+
+// SUSFS 相关变量
+#ifdef CONFIG_KSU_SUSFS_SUS_SU
+bool ksu_devpts_hook = false;
+EXPORT_SYMBOL_GPL(ksu_devpts_hook);
+bool susfs_is_sus_su_hooks_enabled __read_mostly = false;
+int susfs_sus_su_working_mode = 0;
+extern bool ksu_su_compat_enabled;
+#endif
+
+// 常量定义，优化内存使用
+static const char sh_path[] = SH_PATH;
 static const char ksud_path[] = KSUD_PATH;
 static const char su[] = SU_PATH;
 
-// 在用户栈上分配空间并复制数据，返回用户空间指针
+// 在用户栈上分配空间并复制数据
 static inline void __user *userspace_stack_buffer(const void *d, size_t len)
 {
     char __user *p = (void __user *)current_user_stack_pointer() - len;
     return copy_to_user(p, d, len) ? NULL : p;
 }
 
-// 获取 "sh" 路径的用户空间指针
+// 获取 sh 路径的用户空间指针
 static inline char __user *sh_user_path(void)
 {
     return userspace_stack_buffer(sh_path, sizeof(sh_path));
@@ -52,10 +67,14 @@ static inline char __user *ksud_user_path(void)
     return userspace_stack_buffer(ksud_path, sizeof(ksud_path));
 }
 
-// 处理 faccessat 系统调用
-int ksu_handle_faccessat(int *dfd, const char __user **filename_user, int *mode,
-                         int *__unused_flags)
+// 处理 faccessat 系统调用，重定向 su 到 sh
+int ksu_handle_faccessat(int *dfd, const char __user **filename_user, int *mode, int *__unused_flags)
 {
+#ifndef CONFIG_KSU_KPROBES_HOOK
+    if (!ksu_sucompat_non_kp) {
+        return 0;
+    }
+#endif
 #ifndef CONFIG_KSU_SUSFS_SUS_SU
     if (!ksu_is_allow_uid(current_uid().val)) {
         return 0;
@@ -73,8 +92,8 @@ int ksu_handle_faccessat(int *dfd, const char __user **filename_user, int *mode,
     return 0;
 }
 
+// 处理 stat 系统调用（6.1.0+，SUSFS模式）
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(6, 1, 0) && defined(CONFIG_KSU_SUSFS_SUS_SU)
-// 处理 stat 系统调用
 struct filename* susfs_ksu_handle_stat(int *dfd, const char __user **filename_user, int *flags)
 {
     struct filename *name = getname_flags(*filename_user, getname_statx_lookup_flags(*flags), NULL);
@@ -87,16 +106,20 @@ struct filename* susfs_ksu_handle_stat(int *dfd, const char __user **filename_us
         return name;
     }
 
-    const char sh[] = SH_PATH;
     pr_info("vfs_fstatat su->sh!\n");
-    memcpy((void *)name->name, sh, sizeof(sh));
+    memcpy((void *)name->name, sh_path, sizeof(sh_path));
     return name;
 }
 #endif
 
-// 处理 stat 系统调用
+// 处理 stat 系统调用，重定向 su 到 sh
 int ksu_handle_stat(int *dfd, const char __user **filename_user, int *flags)
 {
+#ifndef CONFIG_KSU_KPROBES_HOOK
+    if (!ksu_sucompat_non_kp) {
+        return 0;
+    }
+#endif
 #ifndef CONFIG_KSU_SUSFS_SUS_SU
     if (!ksu_is_allow_uid(current_uid().val)) {
         return 0;
@@ -118,12 +141,16 @@ int ksu_handle_stat(int *dfd, const char __user **filename_user, int *flags)
     return 0;
 }
 
-// 处理 execveat 系统调用
-int ksu_handle_execveat_sucompat(int *fd, struct filename **filename_ptr,
-                                 void *__never_use_argv, void *__never_use_envp,
-                                 int *__never_use_flags)
+// 处理 execveat 系统调用，重定向 su 到 ksud 并提权
+int ksu_handle_execveat_sucompat(int *fd, struct filename **filename_ptr, void *__never_use_argv, void *__never_use_envp, int *__never_use_flags)
 {
     struct filename *filename;
+
+#ifndef CONFIG_KSU_KPROBES_HOOK
+    if (!ksu_sucompat_non_kp) {
+        return 0;
+    }
+#endif
 
     if (unlikely(!filename_ptr))
         return 0;
@@ -148,17 +175,33 @@ int ksu_handle_execveat_sucompat(int *fd, struct filename **filename_ptr,
     return 0;
 }
 
-// 处理 execve 系统调用
-int ksu_handle_execve_sucompat(int *fd, const char __user **filename_user,
-                               void *__never_use_argv, void *__never_use_envp,
-                               int *__never_use_flags)
+// 处理 execve 系统调用，重定向 su 到 ksud 并提权
+int ksu_handle_execve_sucompat(int *fd, const char __user **filename_user, void *__never_use_argv, void *__never_use_envp, int *__never_use_flags)
 {
+#ifndef CONFIG_KSU_KPROBES_HOOK
+    if (!ksu_sucompat_non_kp) {
+        return 0;
+    }
+#endif
+
     char path[sizeof(su) + 1] = {0};
 
     if (unlikely(!filename_user))
         return 0;
 
-    ksu_strncpy_from_user_nofault(path, *filename_user, sizeof(path));
+#if LINUX_VERSION_CODE < KERNEL_VERSION(5,0,0)
+    if (!access_ok(VERIFY_READ, *filename_user, sizeof(path)))
+        return 0;
+#else
+    if (!access_ok(*filename_user, sizeof(path)))
+        return 0;
+#endif
+
+    long len = strncpy_from_user(path, *filename_user, sizeof(path));
+    if (len <= 0 || len > sizeof(path))
+        return 0;
+
+    path[sizeof(path) - 1] = '\0';
 
     if (likely(memcmp(path, su, sizeof(su))))
         return 0;
@@ -173,65 +216,48 @@ int ksu_handle_execve_sucompat(int *fd, const char __user **filename_user,
     return 0;
 }
 
-// 处理 devpts inodes 的安全上下文
-static int ksu_inline_handle_devpts(struct inode *inode)
+// 处理 devpts 的 SELinux 上下文
+int ksu_handle_devpts(struct inode *inode)
 {
-    if (!current->mm) {
+#ifndef CONFIG_KSU_KPROBES_HOOK
+    if (!ksu_sucompat_non_kp) {
         return 0;
     }
-
-    uid_t uid = current_uid().val;
-    if (uid % 100000 < 10000) {
-        return 0;
-    }
-
-    if (!ksu_is_allow_uid(uid))
-        return 0;
-
-    if (ksu_devpts_hook) {
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 1, 0)
-        struct inode_security_struct *sec = selinux_inode(inode);
-#else
-        struct inode_security_struct *sec = (struct inode_security_struct *)inode->i_security;
 #endif
-        if (sec) {
-            sec->sid = ksu_devpts_sid;
-        }
+
+    if (!current->mm || (current_uid().val % 100000 < 10000) || !ksu_is_allow_uid(current_uid().val)) {
+        return 0;
+    }
+
+#ifdef CONFIG_KSU_SUSFS_SUS_SU
+    if (!ksu_devpts_hook) {
+        return 0;
+    }
+#else
+    if (!ksu_devpts_sid) {
+        return 0;
+    }
+#endif
+
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 1, 0)
+    struct inode_security_struct *sec = selinux_inode(inode);
+#else
+    struct inode_security_struct *sec = (struct inode_security_struct *)inode->i_security;
+#endif
+    if (sec) {
+        sec->sid = ksu_devpts_sid;
     }
 
     return 0;
 }
-
-// 包装函数，调用 devpts 处理逻辑
-int ksu_handle_devpts(struct inode *inode)
-{
-    return ksu_inline_handle_devpts(inode);
-}
-
-// 包装函数，调用 devpts 处理逻辑
+// 包装 devpts 处理函数（备用）
 int __ksu_handle_devpts(struct inode *inode)
 {
-    return ksu_inline_handle_devpts(inode);
+    return ksu_handle_devpts(inode);
 }
 
 #ifdef CONFIG_KPROBES
-// 处理 pts_unix98_lookup 的 kprobe 钩子
-static int pts_unix98_lookup_pre(struct kprobe *p, struct pt_regs *regs)
-{
-    struct inode *inode;
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 6, 0)
-    struct file *file = (struct file *)PT_REGS_PARM2(regs);
-    inode = file->f_path.dentry->d_inode;
-#else
-    inode = (struct inode *)PT_REGS_PARM2(regs);
-#endif
-    return ksu_inline_handle_devpts(inode);
-}
-
-static struct kprobe *su_kps[4];
-#endif
-
-// 处理 faccessat 的 kprobe 钩子
+// Kprobe 处理函数：faccessat
 static int faccessat_handler_pre(struct kprobe *p, struct pt_regs *regs)
 {
     struct pt_regs *real_regs = PT_REAL_REGS(regs);
@@ -242,7 +268,7 @@ static int faccessat_handler_pre(struct kprobe *p, struct pt_regs *regs)
     return ksu_handle_faccessat(dfd, filename_user, mode, NULL);
 }
 
-// 处理 newfstatat 的 kprobe 钩子
+// Kprobe 处理函数：newfstatat
 static int newfstatat_handler_pre(struct kprobe *p, struct pt_regs *regs)
 {
     struct pt_regs *real_regs = PT_REAL_REGS(regs);
@@ -253,7 +279,7 @@ static int newfstatat_handler_pre(struct kprobe *p, struct pt_regs *regs)
     return ksu_handle_stat(dfd, filename_user, flags);
 }
 
-// 处理 execve 的 kprobe 钩子
+// Kprobe 处理函数：execve
 static int execve_handler_pre(struct kprobe *p, struct pt_regs *regs)
 {
     struct pt_regs *real_regs = PT_REAL_REGS(regs);
@@ -262,7 +288,22 @@ static int execve_handler_pre(struct kprobe *p, struct pt_regs *regs)
     return ksu_handle_execve_sucompat(AT_FDCWD, filename_user, NULL, NULL, NULL);
 }
 
-// 初始化 kprobe
+// Kprobe 处理函数：pts_unix98_lookup
+static int pts_unix98_lookup_pre(struct kprobe *p, struct pt_regs *regs)
+{
+    struct inode *inode;
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 6, 0)
+    struct file *file = (struct file *)PT_REGS_PARM2(regs);
+    inode = file->f_path.dentry->d_inode;
+#else
+    inode = (struct inode *)PT_REGS_PARM2(regs);
+#endif
+    return ksu_handle_devpts(inode);
+}
+
+static struct kprobe *su_kps[4];
+
+// 初始化 Kprobe
 static struct kprobe *init_kprobe(const char *name, kprobe_pre_handler_t handler)
 {
     struct kprobe *kp = kzalloc(sizeof(struct kprobe), GFP_KERNEL);
@@ -281,7 +322,7 @@ static struct kprobe *init_kprobe(const char *name, kprobe_pre_handler_t handler
     return kp;
 }
 
-// 销毁 kprobe
+// 销毁 Kprobe
 static void destroy_kprobe(struct kprobe **kp_ptr)
 {
     struct kprobe *kp = *kp_ptr;
@@ -292,35 +333,37 @@ static void destroy_kprobe(struct kprobe **kp_ptr)
     kfree(kp);
     *kp_ptr = NULL;
 }
+#endif
 
-// 初始化 sucompat 模块，注册 kprobe 钩子
-void ksu_sucompat_init()
+// 初始化 sucompat 模块，注册 Kprobe 钩子
+void ksu_sucompat_init(void)
 {
 #ifdef CONFIG_KPROBES
     su_kps[0] = init_kprobe(SYS_EXECVE_SYMBOL, execve_handler_pre);
     su_kps[1] = init_kprobe(SYS_FACCESSAT_SYMBOL, faccessat_handler_pre);
     su_kps[2] = init_kprobe(SYS_NEWFSTATAT_SYMBOL, newfstatat_handler_pre);
     su_kps[3] = init_kprobe("pts_unix98_lookup", pts_unix98_lookup_pre);
+#else
+    ksu_sucompat_non_kp = true;
+    pr_info("ksu_sucompat_init: hooks enabled: execve/execveat_su, faccessat, stat, devpts\n");
 #endif
 }
 
-// 清理 sucompat 模块，注销 kprobe 钩子
-void ksu_sucompat_exit()
+// 清理 sucompat 模块，注销 Kprobe 钩子
+void ksu_sucompat_exit(void)
 {
 #ifdef CONFIG_KPROBES
     for (int i = 0; i < ARRAY_SIZE(su_kps); i++) {
         destroy_kprobe(&su_kps[i]);
     }
+#else
+    ksu_sucompat_non_kp = false;
+    pr_info("ksu_sucompat_exit: hooks disabled: execve/execveat_su, faccessat, stat, devpts\n");
 #endif
 }
 
 #ifdef CONFIG_KSU_SUSFS_SUS_SU
-extern bool ksu_su_compat_enabled;
-bool ksu_devpts_hook = false;
-bool susfs_is_sus_su_hooks_enabled __read_mostly = false;
-int susfs_sus_su_working_mode = 0;
-
-// 检查 kprobe 是否启用
+// 检查 Kprobe 是否启用
 static bool ksu_is_su_kps_enabled(void)
 {
     for (int i = 0; i < ARRAY_SIZE(su_kps); i++) {
@@ -331,7 +374,7 @@ static bool ksu_is_su_kps_enabled(void)
     return false;
 }
 
-// 禁用 SUSFS_SUS_SU 钩子
+// 禁用 SUSFS su 钩子
 void ksu_susfs_disable_sus_su(void)
 {
     susfs_is_sus_su_hooks_enabled = false;
@@ -343,7 +386,7 @@ void ksu_susfs_disable_sus_su(void)
     }
 }
 
-// 启用 SUSFS_SUS_SU 钩子
+// 启用 SUSFS su 钩子
 void ksu_susfs_enable_sus_su(void)
 {
     if (ksu_is_su_kps_enabled()) {
